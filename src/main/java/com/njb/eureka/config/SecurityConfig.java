a86bbfe586d7b6ec6e50ef0e96ac2556be779f1f
Nagaraja Balakrishna <nbalakrishna@manh.com>
2020-06-07 20:38:16 +0530

    Spring security to secure eureka

---
+++ src/main/java/com/njb/eureka/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.njb.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.httpBasic();
+	}
+}
